5adfe709c54f56c6ae45e51a4516f8ca158ee2f5
Sekirei <136705426+Sekirei8428@users.noreply.github.com>
2023-11-01 17:01:24 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,59 +1,34 @@
 document.addEventListener("DOMContentLoaded", function() {
     const generateButton = document.getElementById("generateButton");
     const randomFishDonElement = document.getElementById("randomFishDon");
-    const seafoodDonImage = document.getElementById("seafoodDonImage");
+    const menuNameElement = document.getElementById("menuName");
 
     function generateRandomFishDon() {
         return Math.floor(Math.random() * 154) + 1;
     }
 
-    function updateSeafoodDonImage(randomFishDon) {
-        seafoodDonImage.src = "http://uodon.jp/wp-content/themes/uodon-hp/img_menu45/" + randomFishDon + ".jpg";
+    function updateMenuName(randomFishDon) {
+        // menu.jsonファイルを読み込む
+        fetch('./menu.json')
+            .then(response => response.json())
+            .then(data => {
+                // ランダムな番号に対応する名前を取得
+                const menuName = data[randomFishDon];
+
+                if (menuName) {
+                    menuNameElement.textContent = "メニュー名: " + menuName;
+                } else {
+                    menuNameElement.textContent = "メニュー名: メニューがありません";
+                }
+            })
+            .catch(error => {
+                console.error('メニュー情報の読み込み中にエラーが発生しました:', error);
+            });
     }
 
     generateButton.addEventListener("click", function() {
         let randomFishDon = generateRandomFishDon();
-
-        if (standardCheckbox.checked) {
-            while (randomFishDon < 1 || randomFishDon > 54) {
-                randomFishDon = generateRandomFishDon();
-            }
-        } else if (lowPriceCheckbox.checked) {
-            while (randomFishDon < 55 || randomFishDon > 135) {
-                randomFishDon = generateRandomFishDon();
-            }
-        } else if (popularCheckbox.checked) {
-            while (randomFishDon < 136 || randomFishDon > 143) {
-                randomFishDon = generateRandomFishDon();
-            }
-        } else if (volumeCheckbox.checked) {
-            while (randomFishDon < 144 || randomFishDon > 154) {
-                randomFishDon = generateRandomFishDon();
-            }
-        } else {
-            randomFishDon = "チェックボックスを選択してください";
-        }
-
         randomFishDonElement.textContent = randomFishDon;
-        updateSeafoodDonImage(randomFishDon);
-        fetch('./menu.csv')
-        .then(response => response.text())
-        .then(data => {
-            const menuArray = data.split('\n');
-            const menuData = {};
-            menuArray.forEach(menuItem => {
-                const [menuNumber, menuName] = menuItem.split(',');
-                menuData[menuNumber] = menuName;
-            });
-
-            if (menuData[randomFishDon]) {
-                menuNameElement.textContent = "メニュー名: " + menuData[randomFishDon];
-            } else {
-                menuNameElement.textContent = "メニュー名: メニューがありません";
-            }
-        })
-        .catch(error => {
-            console.error('メニュー情報の読み込み中にエラーが発生しました:', error);
-        });
+        updateMenuName(randomFishDon);
     });
 });
